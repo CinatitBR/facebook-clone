782fc6f272d0e72b3456bfe4a268d87030daf932
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-12-25 22:52:14 -0300

    Create IconWrapper

--- src/components/Icons/Icons.styles.ts
+++ src/components/Icons/Icons.styles.ts
@@ -1,9 +1,18 @@
 import styled from 'styled-components'
 
-import { Props } from './Icons'
+import { Props, IconWrapperProps } from './Icons'
 
 export const ProfileContainer = styled.img<Props>`
   width: ${props => `${props.size}px`};
   height: ${props => `${props.size}px`};
   border-radius: 50%;
+`
+export const IconWrapperContainer = styled.div<IconWrapperProps>`
+  width: ${props => `${props.size}px`};
+  height: ${props => `${props.size}px`};
+  border-radius: 50%;
+  background-color: ${props => props.backgroundColor};
+
+  display: grid;
+  place-items: center;
 `
--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -1,4 +1,4 @@
-import { ProfileContainer } from './Icons.styles'
+import { ProfileContainer, IconWrapperContainer } from './Icons.styles'
 
 export interface Props {
   src: string
@@ -11,4 +11,26 @@ export const ProfileIcon = ({ src, size = 36 } : Props) => (
     alt="Profile pic"
     size={size}
   />
+)
+
+export interface IconWrapperProps {
+  children: React.ReactNode
+  size?: number,
+  backgroundColor?: string
+  className?: string
+}
+
+export const IconWrapper = ({ 
+  children, 
+  size = 36, 
+  backgroundColor = 'var(--secondary-button-background)',
+  className
+} : IconWrapperProps) => (
+  <IconWrapperContainer 
+    size={size} 
+    backgroundColor={backgroundColor}
+    className={className} 
+  >
+    {children}
+  </IconWrapperContainer>
 )
