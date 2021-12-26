1ab439b4590d42041cbe065e45f66ff21086d214
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-12-26 20:47:21 -0300

    Add comment section to PostUser

--- src/components/PostUser/PostUser.styles.ts
+++ src/components/PostUser/PostUser.styles.ts
@@ -4,106 +4,113 @@ import { Container as PostWrapperContainer } from '../PostWrapper/PostWrapper.st
 export const Container = styled(PostWrapperContainer)`
   display: flex;
   flex-direction: column;
+  gap: 4px;
+
+  padding: 12px 0;
+
+  .margin {
+    margin: 0 16px;
+  }
+
+  .divider {
+    border-bottom: solid 1px var(--divider);
+  }
+`
+
+export const PostUserHeader = styled.header`
+  display: flex;
+  flex-direction: row;
   gap: 8px;
 
-  padding: 0;
-  padding-top: 12px;
-
-  .padding {
-    padding: 0 16px;
-  }
-
-  header {
-    display: flex;
-    flex-direction: row;
-    gap: 8px;
-  }
-
-  header a:hover {
+  a:hover {
     text-decoration: underline;
   }
 
-  header .info .top {
+  .info .top {
     display: flex;
     flex-direction: row;
     align-items: center;
     gap: 6px;
   }
 
-  header .creator {
+  .creator {
     font-weight: 600;
   }
 
-  header .badge {
-    width: 12px;
-    height: 12px;
-  }
-
-  header .info .bottom {
+  .info .bottom {
     font-size: .8125rem;
     color: var(--secondary-text);
   }
+`
 
-  /* Post image */
-  .postImageWrapper {
-    width: 100%;
-    background-color: black;
+export const TextWrapper = styled.div``
 
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-  }
+export const BadgeIcon = styled.img`
+  width: 12px;
+  height: 12px;
+`
 
-  /* Post info */
-  .postInfo {
-    color: var(--secondary-text);
+export const PostImageWrapper = styled.a`
+  width: 100%;
+  background-color: black;
 
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`
+
+export const PostInfo = styled.div`
+  color: var(--secondary-text);
+  padding: 10px 0;
+
+  display: flex;
+  flex-direction: column;
+
+  .content {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
   }
 
-  .postInfo .left {
+  .left *, .right * {
+    cursor: pointer;
+  }
+
+  .left *:hover,
+  .right *:hover {
+    text-decoration: underline;
+  }
+
+  .left {
     display: flex;
     flex-direction: row;
     align-items: center;
     gap: 8px;
   } 
 
-  .postInfo .left *, 
-  .postInfo .right * {
-    cursor: pointer;
-  }
-
-  .postInfo .left *:hover,
-  .postInfo .right *:hover {
-    text-decoration: underline;
-  }
-
-  .postInfo .right {
+  .right {
     display: flex;
     flex-direction: row;
     gap: 8px;
   }
+`
 
-  .postInfo .right * {
-    cursor: pointer;
-  }
+export const ActionButtons = styled.div`
+  padding-bottom: 4px;
 
-  /* Action buttons */
-  .actionButtons {
-    display: flex;
-    flex-direction: row;
-    gap: 4px;
-  }
+  display: flex;
+  flex-direction: row;
+  gap: 4px;
 
-  .actionButtons button {
+  button {
+    height: 32px;
+    width: 100%;
+    padding: 0;
+    border-radius: 4px;
+
     color: var(--secondary-text);
     font-weight: 600;
     font-size: .9375rem;
-    width: 100%;
-    height: 44px;
-    padding: 0;
     
     display: flex;
     flex-direction: row;
@@ -112,7 +119,78 @@ export const Container = styled(PostWrapperContainer)`
     gap: 8px;
   }
 
-  .actionButtons img {
+  img {
+    filter: var(--filter-secondary-icon);
+  }
+`
+
+export const Footer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+
+  .seeMore {
+    color: var(--secondary-text);
+    font-weight: 600;
+    cursor: pointer;
+  }
+
+  .seeMore:hover {
+    text-decoration: underline;
+  }
+`
+
+export const SelectFilter = styled.div`
+  font-weight: 600;
+  color: var(--secondary-text);
+  cursor: pointer;
+  margin-left: auto;
+  min-height: 28px;
+  
+  display: inline-flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+`
+
+export const InputCommentRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+`
+
+export const InputComment = styled.div`
+  width: 100%;
+  border-radius: 20px;
+  font-size: .9375rem;
+  color: var(--primary-text);
+  background-color: var(--comment-background);
+  padding-left: 12px;
+  padding-top: 2px;
+  padding-bottom: 2px;
+  padding-right: 2px;
+
+  display: flex;
+  flex-direction: row;
+
+  input {
+    color: var(--primary-text);
+    font-size: .9375rem;
+  }
+
+  .buttons {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .buttons .icon {
+    cursor: pointer;
+  }  
+
+  .buttons .icon img {
     filter: var(--filter-secondary-icon);
   }
 `
--- src/components/PostUser/PostUser.tsx
+++ src/components/PostUser/PostUser.tsx
@@ -1,11 +1,30 @@
 import { ProfileIcon } from '../Icons/Icons'
 import {ReactComponent as PublicIcon} from '../../assets/public-icon.svg'
 import {ReactComponent as LikeColoredSvg } from '../../assets/like-colored.svg'
+import { ReactComponent as SortDownSvg } from '../../assets/sort-down-icon.svg'
 import likeImg from '../../assets/like-icon.png'
 import commentImg from '../../assets/comment-icon.png'
 import verifiedIcon from '../../assets/verified-icon.png'
+import profilePic from '../../assets/profile-pic.jpg'
+import { IconWrapper } from '../Icons/Icons'
+import emojiIcon from '../../assets/emoji-comment.png'
+import photoIcon from '../../assets/photo-comment.png'
+import gifIcon from '../../assets/gif-comment.png'
+import stickerIcon from '../../assets/sticker-comment.png'
 
-import { Container } from './PostUser.styles'
+import { 
+  Container, 
+  PostImageWrapper,
+  PostInfo, 
+  ActionButtons, 
+  PostUserHeader, 
+  BadgeIcon,
+  TextWrapper,
+  Footer,
+  SelectFilter,
+  InputCommentRow,
+  InputComment
+} from './PostUser.styles'
 
 interface Props {
   profileImageSrc: string,
@@ -17,6 +36,18 @@ interface Props {
   postImageSrc?: string
 }
 
+const InputCommentButton = ({ iconSrc, alt }: { iconSrc: string, alt: string }) => (
+  <IconWrapper 
+    wrapperSize={32} 
+    iconSize={16}
+    backgroundColor="inherit"
+    backgroundColorHover="var(--hover-overlay)"
+    className="icon"
+  >
+    <img src={iconSrc} alt={alt} />
+  </IconWrapper>  
+)
+
 const PostUser = ({ 
   profileImageSrc, 
   creator, 
@@ -27,16 +58,18 @@ const PostUser = ({
   postImageSrc
 } : Props) => (
   <Container>
-    <header className="padding">
+    <PostUserHeader className="margin">
       <ProfileIcon src={profileImageSrc} />
 
       <div className="info">
         <div className="top">
           <span className="creator">
             <a href="#">{creator}</a>
-          </span> 
+          </span>
 
-          {verified && <img src={verifiedIcon} alt="verified" className="badge"></img>}
+          {verified && 
+            <BadgeIcon src={verifiedIcon} alt="Verified" />
+          }
         </div>
 
         <div className="bottom">
@@ -45,30 +78,32 @@ const PostUser = ({
           }
         </div>
       </div>
-    </header>
+    </PostUserHeader>
 
-    <div className="text padding">
+    <TextWrapper className="margin">
       {text}
-    </div>
+    </TextWrapper>
     
     {postImageSrc &&
-      <a href="#" className="postImageWrapper">
+      <PostImageWrapper href="#">
         <img src={postImageSrc} alt="Post" />
-      </a>
+      </PostImageWrapper>
     }
 
-    <div className="postInfo padding">
-      <div className="left">
-        <LikeColoredSvg width={18} /> <span>528</span>
-      </div>
+    <PostInfo className="margin divider">
+      <div className="content">
+        <div className="left">
+          <LikeColoredSvg width={18} /> <span>528</span>
+        </div>
 
-      <div className="right">
-        <span className="commentCount">721 comments</span>
-        <span className="shareCount">12 shares</span>
+        <div className="right">
+          <span className="commentCount">721 comments</span>
+          <span className="shareCount">12 shares</span>
+        </div>
       </div>
-    </div>
+    </PostInfo>
 
-    <div className="actionButtons padding">
+    <ActionButtons className="margin divider">
       <button>
         <img src={likeImg} alt="Like" /> Like
       </button>
@@ -80,7 +115,48 @@ const PostUser = ({
       <button>
         <img src={likeImg} alt="Like" /> Like
       </button>
-    </div>
+    </ActionButtons>
+
+    <Footer className="margin">
+      <SelectFilter>
+        Most relevant <SortDownSvg />
+      </SelectFilter>
+
+      <InputCommentRow>
+        <ProfileIcon 
+          src={profilePic}
+          online
+        />
+
+        <InputComment>
+          <input type="text" placeholder="Write a comment..." />
+
+          <div className="buttons">
+            <InputCommentButton 
+              iconSrc={emojiIcon}
+              alt="Emoji"
+            />
+
+            <InputCommentButton 
+              iconSrc={photoIcon}
+              alt="Emoji"
+            />
+
+            <InputCommentButton 
+              iconSrc={gifIcon}
+              alt="Emoji"
+            />
+
+            <InputCommentButton 
+              iconSrc={stickerIcon}
+              alt="Emoji"
+            />
+          </div>
+        </InputComment>
+      </InputCommentRow>
+
+      <span className="seeMore">View comments</span>
+    </Footer>
   </Container>
 )
 
