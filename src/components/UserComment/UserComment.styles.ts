7473f2d91300ca3db478f35ca4a435662c7d0149
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-12-27 22:40:42 -0300

    Update component role
    UserComment will be used as a wrapper to thread comments

--- src/components/UserComment/UserComment.styles.ts
+++ src/components/UserComment/UserComment.styles.ts
@@ -14,14 +14,6 @@ export const Container = styled.article<{ level: number }>`
     max-width: calc(100% - 26px);
   }
 
-  /* Comment level 2 and 3 */
-  ${props => props.level >= 2 && css`
-    .profileIcon {
-      padding-left: 6px;
-      margin-top: 8px;
-    }
-  `}
-
   /* Comment level 2 */
   ${props => props.level === 2 && css`
     padding-left: 54px;
@@ -101,61 +93,7 @@ export const ThreadBottom = styled.div<{ level: number }>`
   `}
 `
 
-export const Content = styled.div<{ subComment?: boolean }>`
-  background-color: var(--comment-background);
-  border-radius: 18px;
-  color: var(--primary-text);
-  padding: 8px 12px;
-
-  position: relative;
-`
-
-export const AuthorName = styled.a`
-  font-size: .8125rem;
-  font-weight: 600;
-  cursor: pointer;
-  
-  :hover {
-    text-decoration: underline;
-  }
-`
-
-export const TextWrapper = styled.div`
-  font-size: .9375rem;
-`
-
-export const LikeCountWrapper = styled.div`
-  background-color: var(--popover-background);
-  font-size: .8125rem;
-  color: var(--secondary-text);
-  padding: 2px;
-  border-radius: 10px;
-  box-shadow: 0 1px 3px 0 var(--shadow-2);
-  cursor: pointer;
-
-  display: flex;
-  flex-direction: row;
-  place-items: center;
-  gap: 4px;
-
-  position: absolute;
-  bottom: -15px;
-  right: 0;
-
-  svg {
-    width: 16px;
-    height: 16px;
-  }
-`
-
-export const ActionButtons = styled.div`
-  font-weight: bold;
-  color: var(--secondary-text);
-  font-size: 12px;
-  margin-left: 12px;
-
-  span:hover {
-    cursor: pointer;
-    text-decoration: underline;
-  }
+export const Child = styled.div`
+  padding-left: 6px;
+  margin-top: 8px;
 `
--- src/components/UserComment/UserComment.tsx
+++ src/components/UserComment/UserComment.tsx
@@ -1,63 +1,28 @@
-import { ProfileIcon } from '../Icons/Icons'
-import {ReactComponent as LikeColoredSvg} from '../../assets/like-colored.svg'
-
 import { 
   Container,
-  Content,
-  AuthorName,
-  TextWrapper,
-  LikeCountWrapper,
-  ActionButtons,
   ThreadMain,
   ThreadSubMain,
   ThreadLeft,
-  ThreadBottom
+  ThreadBottom,
+  Child
 } from './UserComment.styles'
 
 export type Props = {
-  comment: {
-    authorPicSrc: string,
-    authorName: string,
-    text: string,
-    likeCount: number,
-    createdAt: string,
-    level: 0 | 1 | 2 | 3 | 4
-  },
-  last?: boolean
+  level: 0 | 1 | 2 | 3 | 4
+  children: React.ReactNode,
+  last: boolean
 } 
 
-const UserComment = ({ comment, last }: Props) => {
-  const size = (comment.level >= 2) ? 24 : 32
+const UserComment = ({ level, children, last }: Props) => {
+  return <Container level={level}>
+    {(level >= 2 && level < 4) && <ThreadMain level={level} />}
+    {(level === 3 && !last) && <ThreadSubMain level={level} />}
+    {(level >= 2) && <ThreadLeft level={level} />}
+    {(level >= 1 && level < 3 && !last) && <ThreadBottom level={level} />}
 
-  return <Container level={comment.level}>
-    {(comment.level >= 2 && comment.level < 4) && <ThreadMain level={comment.level} />}
-    {(comment.level === 3 && !last) && <ThreadSubMain level={comment.level} />}
-    {(comment.level >= 2) && <ThreadLeft level={comment.level} />}
-    {(comment.level >= 1 && comment.level < 3 && !last) && <ThreadBottom level={comment.level} />}
-
-    <ProfileIcon 
-      src={comment.authorPicSrc} 
-      className="profileIcon" 
-      size={size}
-    />
-
-    <div className="right">
-      <Content>
-        <AuthorName>{comment.authorName}</AuthorName>
-
-        <TextWrapper>{comment.text}</TextWrapper>
-
-        <LikeCountWrapper>
-          <LikeColoredSvg /> <span>{comment.likeCount}</span>
-        </LikeCountWrapper>
-      </Content>
-
-      <ActionButtons>
-        <span>Like</span>&nbsp;·&nbsp;
-        <span>Reply</span>&nbsp;·&nbsp;
-        <span style={{ fontWeight: 400 }}>{comment.createdAt}</span>
-      </ActionButtons>
-    </div>
+    <Child>
+      {children}
+    </Child>
   </Container>
 }
 
